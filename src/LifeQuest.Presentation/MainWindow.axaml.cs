e62739a39d947a08b613cd8aea726f8438417a47
Sviatoslav <sviatoslavpavlenko3@gmail.com>
2026-04-05 17:41:18 +0300

    INPR-19 feat: created Architecture solution, projects  and dependencies

---
+++ src/LifeQuest.Presentation/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace LifeQuest.Presentation;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
