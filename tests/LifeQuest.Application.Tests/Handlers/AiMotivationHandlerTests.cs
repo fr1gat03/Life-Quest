53f6bdd9403fdad85f49c78233138538053618f2
DNS <dnsbrawler222@gmail.com>
2026-04-29 11:34:25 +0300

    TODO-45 Add tests to the application layer

---
+++ tests/LifeQuest.Application.Tests/Handlers/AiMotivationHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using LifeQuest.Application.Handlers.QuestExecution;
+using LifeQuest.Application.Tests.Fakes;
+using LifeQuest.Application.Tests.Helpers;
+
+namespace LifeQuest.Application.Tests.Handlers;
+
+public class AiMotivationHandlerTests
+{
+    private AiMotivationHandler _handler = null;
+
+    [SetUp]
+    public void Setup() => _handler = new AiMotivationHandler(new FakeAiService());
+
+    [Test]
+    public async Task Handle_WritesMotivationMessageToContext()
+    {
+        var context = new QuestExecutionContext(TestData.NewQuest(), TestData.NewUser());
+
+        await _handler.Handle(context);
+
+        Assert.That(context.MotivationMessage, Is.EqualTo(FakeAiService.FakeMessage));
+    }
+
+    [Test]
+    public async Task Handle_ReturnsSuccessWithMotivationMessage()
+    {
+        var context = new QuestExecutionContext(TestData.NewQuest(), TestData.NewUser());
+
+        QuestExecutionResult result = await _handler.Handle(context);
+
+        Assert.That(result.IsSuccess, Is.True);
+        Assert.That(result.MotivationMessage, Is.EqualTo(FakeAiService.FakeMessage));
+    }
+}
---
+++ tests/LifeQuest.Application.Tests/Helpers/TestData.cs
@@ -0,0 +1,17 @@
+﻿using LifeQuest.Domain.Components;
+using LifeQuest.Domain.Entities;
+using LifeQuest.Domain.Enums;
+
+namespace LifeQuest.Application.Tests.Helpers;
+
+public static class TestData
+{
+    public static Quest NewQuest(bool isCompleted = false)
+    {
+        var quest = new Quest("Тестовий квест", rewardXp: 50, rewardGold: 10, Difficulty.Medium);
+        if (isCompleted) quest.ToComplete();
+        return quest;
+    }
+
+    public static User NewUser() => new(1, "hero", "hash");
+}
