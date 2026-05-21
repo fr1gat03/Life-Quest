9982fb4515e2568ddf9a0db2eff66dc8661a1237
DNS <dnsbrawler222@gmail.com>
2026-05-22 00:22:47 +0300

    TODO-69 feat: add e2e tests.

---
+++ tests/LifeQuest.EndToEnd.Tests/PlayerJourneyE2ETests.cs
@@ -0,0 +1,58 @@
+﻿using LifeQuest.Domain.Entities;
+using LifeQuest.Domain.Enums;
+using User = LifeQuest.Domain.Entities.User;
+
+namespace LifeQuest.E2E.Tests
+{
+    public class PlayerJourneyE2ETests
+    {
+        [Test]
+        public async Task NewPlayer_RegisterLoginCreateAndCompleteQuest_ProgressPersistedAcrossSessions()
+        {
+            var app = new TestApp();
+            int userId;
+            string questId;
+
+            using (var session1 = app.NewSession())
+            {
+                userId = TestApp.NewId();
+                var user = new User(userId, "user", string.Empty);
+                user.SetPassword("Password9090!");
+                session1.UserRepository.SaveUser(user);
+
+                var quest = new Quest(Guid.NewGuid().ToString(),
+                    "Виконати завдання", 80, 40, Difficulty.Medium, userId);
+
+                session1.QuestRepository.UpdateQuest(quest);
+                questId = quest.Id;
+            }
+
+            using (var session2 = app.NewSession())
+            {
+                var loggedIn = session2.UserRepository.GetUserByLogin("user");
+                Assert.That(loggedIn, Is.Not.Null);
+                Assert.That(loggedIn.VerifyPassword("Password9090!"), Is.True);
+
+                var active = session2.QuestRepository.GetActiveQuests(userId).ToList();
+                Assert.That(active.Count, Is.EqualTo(1));
+
+                var result = await session2.QuestService.CompleteQuestAsync(active[0], loggedIn);
+                Assert.That(result.IsSuccess, Is.True);
+                Assert.That(result.MotivationMessage, Does.Contain("Виконати завдання"));
+            }
+
+            using (var session3 = app.NewSession())
+            {
+                var finalUser = session3.UserRepository.GetUserById(userId);
+                Assert.That(finalUser.XP, Is.EqualTo(80));
+                Assert.That(finalUser.Gold, Is.EqualTo(40));
+
+                var completedQuest = session3.QuestRepository.GetQuestById(questId);
+                Assert.That(completedQuest.IsCompleted, Is.True);
+
+                Assert.That(session3.QuestRepository.GetActiveQuests(userId).Any(), Is.False);
+                Assert.That(session3.QuestRepository.GetCompletedQuestsCount(userId), Is.EqualTo(1));
+            }
+        }
+    }
+}
---
+++ tests/LifeQuest.EndToEnd.Tests/ResetProgressE2ETests.cs
@@ -0,0 +1,86 @@
+﻿using LifeQuest.Domain.Entities;
+using LifeQuest.Domain.Enums;
+
+namespace LifeQuest.E2E.Tests
+{
+    public class ResetProgressE2ETests
+    {
+        [Test]
+        public async Task PlayerWithProgress_ResetsEverything_CleanStateSurvivesRestart()
+        {
+            var app = new TestApp();
+            var userId = TestApp.NewId();
+
+            using (var session1 = app.NewSession())
+            {
+                var user = new User(userId, "user", string.Empty);
+                user.SetPassword("Password1010!");
+                session1.UserRepository.SaveUser(user);
+
+                var quest = new Quest(Guid.NewGuid().ToString(),
+                    "Виконати завдання", 100, 50, Difficulty.Hard, userId);
+
+                session1.QuestRepository.UpdateQuest(quest);
+
+                var result = await session1.QuestService.CompleteQuestAsync(quest, user);
+                Assert.That(result.IsSuccess, Is.True);
+            }
+
+            using (var session2 = app.NewSession())
+            {
+                var user = session2.UserRepository.GetUserById(userId);
+                Assert.That(user.XP, Is.EqualTo(100));
+                Assert.That(session2.QuestRepository.GetCompletedQuestsCount(userId), Is.EqualTo(1));
+
+                user.ResetProgress();
+                session2.UserRepository.SaveUser(user);
+                session2.QuestRepository.DeleteAllUserQuests(userId);
+            }
+
+            using (var session3 = app.NewSession())
+            {
+                var user = session3.UserRepository.GetUserById(userId);
+
+                Assert.That(user, Is.Not.Null);
+                Assert.That(user.XP, Is.EqualTo(0));
+                Assert.That(user.Gold, Is.EqualTo(0));
+                Assert.That(user.Level, Is.EqualTo(1));
+                Assert.That(user.Streak, Is.EqualTo(0));
+
+                Assert.That(session3.QuestRepository.HasAnyQuests(userId), Is.False);
+                Assert.That(session3.QuestRepository.GetCompletedQuestsCount(userId), Is.EqualTo(0));
+            }
+        }
+
+        [Test]
+        public void DeletingOnePlayersQuests_DoesNotAffectAnother_AcrossRestart()
+        {
+            var app = new TestApp();
+            var user1Id = TestApp.NewId();
+            var user2Id = TestApp.NewId();
+
+            using (var session1 = app.NewSession())
+            {
+                var user1 = new User(user1Id, "user1", string.Empty);
+                user1.SetPassword("Password1010!");
+                session1.UserRepository.SaveUser(user1);
+
+                var user2 = new User(user2Id, "user2", string.Empty);
+                user2.SetPassword("Password1010!");
+                session1.UserRepository.SaveUser(user2);
+
+                session1.QuestRepository.UpdateQuest(new Quest(Guid.NewGuid().ToString(), "A1", 50, 20, Difficulty.Easy, user1Id));
+                session1.QuestRepository.UpdateQuest(new Quest(Guid.NewGuid().ToString(), "A2", 50, 20, Difficulty.Easy, user1Id));
+                session1.QuestRepository.UpdateQuest(new Quest(Guid.NewGuid().ToString(), "B1", 50, 20, Difficulty.Easy, user2Id));
+
+                session1.QuestRepository.DeleteAllUserQuests(user1Id);
+            }
+
+            using (var session2 = app.NewSession())
+            {
+                Assert.That(session2.QuestRepository.HasAnyQuests(user1Id), Is.False);
+                Assert.That(session2.QuestRepository.GetActiveQuests(user2Id).Count(), Is.EqualTo(1));
+            }
+        }
+    }
+}
